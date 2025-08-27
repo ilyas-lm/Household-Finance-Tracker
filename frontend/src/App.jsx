324f58d4923afdae3573605dc3a958cb9de00837
ilyas-lm <ilyaslambaitil8@gmail.com>
2025-08-27 12:27:15 +0100

    Initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     <h1 className="text-3xl font-bold underline text-blue-600">
+  Tailwind is working!
+</h1>
+
+    </>
+  )
+}
+
+export default App
